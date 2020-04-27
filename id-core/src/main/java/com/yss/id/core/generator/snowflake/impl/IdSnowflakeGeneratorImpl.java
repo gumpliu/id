5468712e187668fc02758e9f455238a7d5935c50
刘胜鹏 <liushengpeg@tftiancai.com>
2020-04-27 16:05:37 +0800

    snowflake format参数验证

--- id-core/src/main/java/com/yss/id/core/generator/snowflake/impl/IdSnowflakeGeneratorImpl.java
+++ id-core/src/main/java/com/yss/id/core/generator/snowflake/impl/IdSnowflakeGeneratorImpl.java
@@ -1,6 +1,7 @@
 package com.yss.id.core.generator.snowflake.impl;
 
 import com.yss.id.core.constans.IDFormatEnum;
+import com.yss.id.core.exception.IdException;
 import com.yss.id.core.generator.snowflake.SnowflakeBufferIdGenerator;
 import com.yss.id.core.generator.snowflake.IdSnowflakeGenerator;
 import com.yss.id.core.service.IdService;
@@ -20,6 +21,11 @@ public class IdSnowflakeGeneratorImpl extends SnowflakeBufferIdGenerator impleme
 
     @Override
     public String getSnowflakeNextId(IDFormatEnum format) {
+
+        if(format == null){
+            throw new IdException("getSnowflakeNextId format Can not be empty ！！");
+        }
+
         return nextId(format);
     }
 }
